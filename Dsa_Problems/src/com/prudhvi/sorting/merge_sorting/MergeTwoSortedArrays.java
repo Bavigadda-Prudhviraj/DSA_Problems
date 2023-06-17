bf3646b9d290e792ad94cbc55d2ae15979dda6ba
Bavigadda <prudhviraj729@gmail.com>
2023-06-17 17:02:16 +0530

    Merge TwoSorted Arrays

---
+++ Dsa_Problems/src/com/prudhvi/sorting/merge_sorting/MergeTwoSortedArrays.java
@@ -0,0 +1,72 @@
+package com.prudhvi.sorting.merge_sorting;
+
+import java.util.Arrays;
+
+public class MergeTwoSortedArrays {
+	/*
+	 Given two sorted integer arrays A and B, merge B and A as one sorted array and return it as an output
+	 Inputs: A = [4, 7, 9]
+  			 B = [2, 11, 19]
+	Output:[2, 4, 7, 9, 11, 19]
+	
+	Hint: Use of additional space is allowed. So, maybe you should try collecting the output in a separate array.
+		  Note:
+				You need two pointers at the head of each array,
+				and you need to compare the values at the head of the arrays to get the current minimum.
+
+				Since A is sorted, all values after the pointer are going to be bigger.
+				Since B is sorted, all values after the pointer are going to be bigger.
+
+				All values before the pointer have already been put in the result array.
+				So, all you need to do is to choose the smaller value from the two heads and move on
+		  Corner cases:
+				what if pointer 1 reaches the end of the array first?
+				What if pointer 2 reaches the end of the array first?
+
+				If pointer 1 reaches the end we can just keep on putting the elements from B
+				in the result array while the pointer 2 does not reach the end.
+				The same process goes for if pointer 2 reaches the end.
+	  
+	 */
+
+	public static void main(String[] args) {
+		int[] A = {4, 7, 9};
+	  	int[] B = {2, 11, 19};
+	  	int n=A.length;
+		int m=B.length;
+		int[] ansArr=new int[m+n];
+		int p1=0;
+		int p2=0;
+		int i=0;
+		while (p1<n && p2<m){
+			if(A[p1]<=B[p2]) {
+				ansArr[i]=A[p1];
+				p1++;
+				i++;
+				
+			}
+			else if(A[p1]>B[p2]) {
+				ansArr[i]=B[p2];
+				p2++;
+				i++;
+			}
+			
+			
+		}
+		while(p1<n) {
+			ansArr[i]=A[p1];
+			p1++;
+			i++;	
+		}
+		while(p2<m) {
+			ansArr[i]=B[p2];
+			p2++;
+			i++;
+		}
+		
+		System.out.println(Arrays.toString(ansArr));
+		
+
+	}
+
+}
