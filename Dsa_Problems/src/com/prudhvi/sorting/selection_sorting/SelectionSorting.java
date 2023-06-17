3f9aa7ad5f6114d94880a2b07763435bd1fdcc9a
Bavigadda <prudhviraj729@gmail.com>
2023-06-18 00:13:38 +0530

    Selection Sorting

--- Dsa_Problems/src/com/prudhvi/sorting/selection_sorting/SelectionSorting.java
+++ Dsa_Problems/src/com/prudhvi/sorting/selection_sorting/SelectionSorting.java
@@ -1,6 +1,6 @@
-package com.prudhvi.recursion;
+package com.prudhvi.sorting.selection_sorting;
 
-public class Sorting {
+public class SelectionSorting {
 
 	public static void main(String[] args) {
 		int[] A= {8,16,80,55,32,8,38,40,65,18,15,45,50,38,54,52,23,74,81,42,28,16,66,35,91,36,44,9,85,58,59,49,75,20,87,60,17,11,39,62,20,17,46,26,81,92};
