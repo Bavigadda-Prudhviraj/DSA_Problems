590d594b54320f7940f985484314f435ef8b3566
Bavigadda <prudhviraj729@gmail.com>
2023-07-29 19:50:18 +0530

    TreeNode

---
+++ Dsa_Problems/src/com/prudhvi/trees/TreeNode.java
@@ -0,0 +1,50 @@
+package com.prudhvi.trees;
+
+import java.util.ArrayList;
+
+public  class TreeNode  implements TreeNodeInterface{
+	int val;
+	TreeNode left;
+	TreeNode right;
+	public TreeNode(int val) {
+		this.val=val;
+		this.left=null;
+		this.right=null;
+	}
+
+	@Override
+	public int size() {
+		
+		return 0;
+	}
+
+	public TreeNode preOrder(ArrayList<Integer> arr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeNode inOrder(ArrayList<Integer> arr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TreeNode postOrder(ArrayList<Integer> arr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int height() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int depth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
