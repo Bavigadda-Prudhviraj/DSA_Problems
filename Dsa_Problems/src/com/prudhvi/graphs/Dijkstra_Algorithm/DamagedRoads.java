dcc17440c0bfcb849cf61053ee810c4c85b1bc93
Bavigadda <prudhviraj729@gmail.com>
2023-10-05 21:21:28 +0530

    Damaged roads

---
+++ Dsa_Problems/src/com/prudhvi/graphs/Dijkstra_Algorithm/DamagedRoads.java
@@ -0,0 +1,11 @@
+package com.prudhvi.graphs.Dijkstra_Algorithm;
+//Damaged roads
+public class DamagedRoads {
+
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3};
+		int[] b = {4, 5, 6};
+
+	}
+
+}
