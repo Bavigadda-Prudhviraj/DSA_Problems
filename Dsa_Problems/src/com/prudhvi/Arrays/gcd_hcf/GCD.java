ef33d3869b9e7938e79bbb142f756439b8d89c11
Bavigadda <prudhviraj729@gmail.com>
2023-11-01 22:25:31 +0530

    All Pair GCD

--- Dsa_Problems/src/com/prudhvi/Arrays/gcd_hcf/GCD.java
+++ Dsa_Problems/src/com/prudhvi/Arrays/gcd_hcf/GCD.java
@@ -83,6 +83,23 @@ public class GCD {
 			}
 		return gcd(Y % X, X);
 	}
+	//Another way
+	public int[] solve(int[] A) {
+
+        int orignalN =(int) Math.sqrt(A.length);//size of original array
+        int[] ans=new int[orignalN]; // original array to return
+        int max=0;
+        int j =0;
+        for(int i =0; i<orignalN;i++){
+            for( ;j<(i+1)*orignalN;j++){
+                max=Math.max(max,A[j]);
+            }
+           
+            ans[i]=max;
+            max=0;
+        }
+        return ans;
+    }
 		
 
 	
